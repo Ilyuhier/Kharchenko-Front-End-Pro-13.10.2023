763ff10fc0da4da444c04705763ee2f65f3fa3f2
Ilyuhier <illia.kharchenko.rs@gmail.com>
2023-10-15 01:49:47 +0300

    Дз 5

--- scrypt.js
+++ scrypt.js
@@ -1,15 +1,3 @@
-let xIn = prompt('Введіть 1 число',); //вводимо перше число
-let yIn = prompt('Введіть 2 число',); //вводимо друге число
-let x = Number(xIn);  //перетворюємо їх
-let y = Number(yIn);
-let plus = x+y;  //робимо всі обрахунки
-let minus = x-y;
-let multiply = x*y;
-let divide = x/y;
-
-alert (`Користувач ввів ${x} і ${y}:
-
-${x}+${y}=${plus}
-${x}-${y}=${minus}
-${x}*${y}=${multiply}
-${x}/${y}=${divide}`); //виводимо результат
+let hours = prompt('Введіть кількість годин',); //дізнаємось кількість годин
+let sec = hours*3600; //перетворюємо в секунди
+alert (`Ваша кількість секунд ${sec}`); //виводимо
