a79273fa650933e91a68db1e731d2bfdffca34d1
Ilyuhier <illia.kharchenko.rs@gmail.com>
2023-10-15 00:44:14 +0300

    Дз 4

--- scrypt.js
+++ scrypt.js
@@ -1,28 +1,15 @@
-//Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+let xIn = prompt('Введіть 1 число',); //вводимо перше число
+let yIn = prompt('Введіть 2 число',); //вводимо друге число
+let x = Number(xIn);  //перетворюємо їх
+let y = Number(yIn);
+let plus = x+y;  //робимо всі обрахунки
+let minus = x-y;
+let multiply = x*y;
+let divide = x/y;
 
-let first_line = prompt('write first line',);
-let second_line = prompt('write second line',);
-let third_line = prompt('write third line',);
-alert(first_line + ' ' + second_line + ' ' + third_line);
+alert (`Користувач ввів ${x} і ${y}:
 
-//Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
-
-//перший спосіб
-
-const numberToString = 52135;
-const string = String(numberToString);  //перетворюємо тип number на string
-const digits = string.split('');  //розділяємо рядок на окремі символи
-const result = digits.join(' ');  //об'єднуємо символи, розділяючи їх пробілами
-
-alert(result); 
-
-//другий спосіб
-
-const number = 65453;
-let n1 = number%10;   //отримуємо остачею перше число
-let n2 = ((number-n1)%100)/10;    //відіймаємо остачу, отримуємо нову, прибираємо нулі
-let n3 = ((number-n1-n2*10)%1000)/100;     //повторюємо каскадом і надалі
-let n4 = ((number-n1-n2*10-n3*100)%10000)/1000;
-let n5 = ((number-n1-n2*10-n3*100-n4*1000)%100000)/10000;
-
-alert (`${n5} ${n4} ${n3} ${n2} ${n1}`);
+${x}+${y}=${plus}
+${x}-${y}=${minus}
+${x}*${y}=${multiply}
+${x}/${y}=${divide}`); //виводимо результат
