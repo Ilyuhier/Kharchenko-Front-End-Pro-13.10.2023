821aefcd4319647888e050cbb48a110d8bfa80fc
Ilyuhier <illia.kharchenko.rs@gmail.com>
2023-10-19 14:26:15 +0300

    Дз 7

--- scrypt.js
+++ scrypt.js
@@ -1,56 +1,17 @@
-const user={};
-user.BirthYear = prompt('В якому році ви народились?');
-if (user.BirthYear == undefined || user.BirthYear == '') {
-  alert(`Шкода, що Ви не захотіли ввести свій рік народження`);
-}
-user.City = prompt('В якому місті ви живете?');
-if(user.City == undefined || user.City == ''){
-  alert(`Шкода, що Ви не захотіли ввести своє місто`);
-}
-user.FavSport = prompt('Який ваш улюблений вид спорту?');
-if(user.FavSport == undefined || user.FavSport == ''){
-  alert(`Шкода, що Ви не захотіли ввести свій улюблений вид спорту`);
-}
-let CityMessage ='';
-let sportMessage ='';
-const currentYear = 2023;
-user.Age = currentYear-user.BirthYear;
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-switch (user.City) {
-  case 'Київ':
-  case 'Kyiv':
-    CityMessage = (`Ти живеш у столиці України`);
-    break;
-  case 'Вашингтон':
-  case 'Vashington':
-    CityMessage = (`Ти живеш у столиці США`);
-    break;
-  case 'Лондон':
-  case 'London':
-    CityMessage = (`Ти живеш у столиці Англії`);
-    break;
-  default:
-    CityMessage = (`Ти живеш у місті ${user.City}`);
-    break;
+if (numOrStr === null) {
+  console.log('ви скасували');
+} else {
+  switch (true) {
+    case numOrStr.trim() === '':
+      console.log('Empty String');
+      break;
+    case isNaN(Number(numOrStr)):
+      console.log('number is Ba_NaN');
+      break;
+    default:
+      console.log('OK!');
+  }
 }
-
-switch (user.FavSport) {
-  case 'Футбол':
-    sportMessage =(`Круто! Хочеш стати Кріштіану Роналду?`);
-    break;
-  case 'Пауерліфтінг':
-    sportMessage =(`Круто! Хочеш стати Хафтором Бйорсоном?`);
-    break;
-  case 'Баскетбол':
-    sportMessage =(`Круто! Хочеш стати Майклом Джорданом?`);
-    break;
-  default :
-    sportMessage ='';
-    break;
-}
-
-alert (
-  `Тобі ${user.Age} років 
-${CityMessage}
-${sportMessage}`
-);
