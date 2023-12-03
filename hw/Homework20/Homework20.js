a5ef61e068a3af2217fa1254176bb5b10c9f692f
Ilyuhier <illia.kharchenko.rs@gmail.com>
2023-12-03 22:14:15 +0200

    Дз 20 виправлене

--- hw/Homework20/Homework20.js
+++ hw/Homework20/Homework20.js
@@ -6,14 +6,20 @@ const http = document.querySelector('#http');
 http.addEventListener('click', httpCheck);
 const https = document.querySelector('#https');
 https.addEventListener('click', httpsCheck);
-const httpUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-const httpsUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+let httpUrl = 'youtube.com/watch?v=dQw4w9WgXcQ';
+let httpsUrl = 'youtube.com/watch?v=dQw4w9WgXcQ';
 function httpCheck() {
   const urlArray = Array.from(httpUrl)
   if (urlArray[0] === 'h' && urlArray[1] === 't' && urlArray[2] === 't' && urlArray[3] === 'p' && urlArray[4] === ':'){
     window.open(httpUrl);
   } else {
-    alert('Це не http протокол')
+    alert('додано http протокол')
+    const httpProtocol = ['h','t','t','p',':','/','/'];
+    urlArray.forEach(element => {
+      httpProtocol.push(element);
+    });
+    httpUrl = httpProtocol.join('');
+    window.open(httpUrl);
   }
 }
 function httpsCheck() {
@@ -22,7 +28,13 @@ function httpsCheck() {
   if (urlArray[0] === 'h' && urlArray[1] === 't' && urlArray[2] === 't' && urlArray[3] === 'p' && urlArray[4] === 's' && urlArray[5] === ':'){
     window.open(httpsUrl);
   } else {
-    alert('Це не https протокол')
+    alert('Додано https протокол')
+    const httpsProtocol = ['h','t','t','p','s',':','/','/'];
+    urlArray.forEach(element => {
+      httpsProtocol.push(element);
+    });
+    httpsUrl = httpsProtocol.join('')
+    window.open(httpsUrl);
   }
 } 
 
